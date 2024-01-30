78d6f257065a35539e0a37f0ee3811179f175d7b
selvakumar <selvakumar.k@gmail.com>
2024-01-30 14:37:55 +0530

    New Dependency

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"d09e354d-2d7d-4439-87c5-e65da350af6d","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706599030278,"group":1,"content":"","tags":"","end":1706599046320,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"e960fedb-4534-4bca-96bd-13564e7fa1db","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706605286633,"group":1,"content":"","tags":"","end":1706605301984,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
