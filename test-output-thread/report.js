0faa39ba965ee851232cba32324cadf050ad3090
selvakumar <selvakumar.k@gmail.com>
2024-01-30 12:22:19 +0530

    Done

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"3d52924f-d833-4c64-b978-77a7f028f6c4","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706593095723,"group":1,"content":"","tags":"","end":1706593112363,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
